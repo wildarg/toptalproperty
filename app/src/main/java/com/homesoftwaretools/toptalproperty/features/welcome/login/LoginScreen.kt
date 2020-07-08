d6f6bc357b02aff07ddea49e2edd79687141a3dd
WildArg <wildarg@gmail.com>
2020-07-08 21:27:02 +0800

    Add credentials check if empty before login

--- app/src/main/java/com/homesoftwaretools/toptalproperty/features/welcome/login/LoginScreen.kt
+++ app/src/main/java/com/homesoftwaretools/toptalproperty/features/welcome/login/LoginScreen.kt
@@ -2,34 +2,43 @@ package com.homesoftwaretools.toptalproperty.features.welcome.login
 
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.homesoftwaretools.toptalproperty.R
 import com.homesoftwaretools.toptalproperty.core.navigator.Navigator
 import com.homesoftwaretools.toptalproperty.core.navigator.Routes
 import com.homesoftwaretools.toptalproperty.core.ui.BaseFragment
 import com.homesoftwaretools.toptalproperty.core.ui.BaseViewModel
 import com.homesoftwaretools.toptalproperty.core.ui.onClick
+import com.homesoftwaretools.toptalproperty.core.utils.ResourceProvider
 import com.homesoftwaretools.toptalproperty.core.utils.Toaster
-import com.homesoftwaretools.toptalproperty.logd
 import org.koin.core.inject
 
 class LoginFragment : BaseFragment() {
 
     override val layoutId: Int = R.layout.login_screen
     private val vm: LoginViewModel by scopedViewModel()
-    lateinit var email: EditText
-    lateinit var password: EditText
+    lateinit var email: TextInputEditText
+    lateinit var emailLayout: TextInputLayout
+    lateinit var password: TextInputEditText
+    lateinit var passwordLayout: TextInputLayout
     lateinit var loginButton: View
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         email = view.findViewById(R.id.email_text)
+        emailLayout = view.findViewById(R.id.email_layout)
         password = view.findViewById(R.id.password_text)
+        passwordLayout = view.findViewById(R.id.password_layout)
         loginButton = view.findViewById(R.id.login_button)
 
         loginButton.onClick {
             vm.logIn(email.text.toString(), password.text.toString())
         }
+        vm.nameError.onChange { emailLayout.error = it }
+        vm.passwordError.onChange { passwordLayout.error = it }
     }
 }
 
@@ -38,13 +47,27 @@ class LoginViewModel(scopeID: String) : BaseViewModel(scopeID) {
     private val navigator: Navigator by scope.inject()
     private val toaster: Toaster by scope.inject()
     private val useCase: LoginUseCase by inject()
+    private val rp: ResourceProvider by inject()
+
+    val nameError: LiveData<String> = MutableLiveData()
+    val passwordError: LiveData<String> = MutableLiveData()
 
     fun logIn(name: String, password: String) {
+        if (checkIfEmpty(name, nameError) || checkIfEmpty(password, passwordError))
+            return
+
         useCase.login(name, password)
             .bindSubscribe(
                 onSuccess = { navigator.push(Routes.DASHBOARD) },
                 onError = { toaster.toast(it.message) }
             )
+    }
+
+    private fun checkIfEmpty(src: String, liveData: LiveData<String>): Boolean {
+        return src.isBlank().apply {
+            (liveData as MutableLiveData)
+                .postValue(if (this) rp.string(R.string.required_field_error) else "")
+        }
     }
 
 }
